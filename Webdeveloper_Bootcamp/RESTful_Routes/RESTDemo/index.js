9d24cfa79c3417c6e2f32d3b7c16157e077e4be0
Rupesh Biswas <rupeshbiswas1997@gmail.com>
2023-01-20 01:00:49 +0530

    RESTful Routes Done

---
+++ Webdeveloper_Bootcamp/RESTful_Routes/RESTDemo/index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, "views"));
+
+// Our fake database:
+let comments = [
+    {
+        id: uuid(),
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        id: uuid(),
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        id: uuid(),
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        id: uuid(),
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+})
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id: uuid() });
+    res.redirect('/comments');
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    if (comment) {
+        res.render('comments/show', { ...comment })
+    } else {
+        res.send("Invalid ID");
+    }
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    if (foundComment) {
+        foundComment.comment = newCommentText;
+        res.redirect('/comments');
+    } else {
+        res.send('Comment not found');
+    }
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const foundComment = comments.find(c => c.id === id);
+    res.render('comments/edit', { ...foundComment })
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+})
+
+
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response");
+})
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send(`OK, here is your order for ${qty} ${meat} tacos`);
+})
+
+app.listen(3000, () => {
+    console.log("ON PORT 3000!");
+})
