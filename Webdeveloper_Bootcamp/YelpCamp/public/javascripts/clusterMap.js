ff42e2b15588de28e9c493f36a78e166d6963cab
Rupesh Biswas <rupeshbiswas1997@gmail.com>
2023-03-01 22:41:19 +0530

    Updated cluster map center to Andaman

--- Webdeveloper_Bootcamp/YelpCamp/public/javascripts/clusterMap.js
+++ Webdeveloper_Bootcamp/YelpCamp/public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
     container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/light-v11',
-    center: [-103.5917, 40.6699],
+    center: [92.9826593163722, 11.457945878847005],
     zoom: 3
 });
 
