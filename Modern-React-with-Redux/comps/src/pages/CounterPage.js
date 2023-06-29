1333b467f36be3f57206a32c112a1117d7eb0675
Rupesh Biswas <rupeshbiswas1997@gmail.com>
2023-06-29 23:11:19 +0530

    section-18-done

--- Modern-React-with-Redux/comps/src/pages/CounterPage.js
+++ Modern-React-with-Redux/comps/src/pages/CounterPage.js
@@ -1,17 +1,82 @@
+import { produce } from "immer";
+import { useReducer } from "react";
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
+import Panel from "../components/Panel";
+
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const SET_VALUE_TO_ADD = "changeValueToAdd";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
+
+function reducer(state, action) {
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            state.count = state.count + action.payload
+            return;
+        case DECREMENT_COUNT:
+            state.count = state.count - action.payload
+            return;
+        case SET_VALUE_TO_ADD:
+            state.valueToAdd = action.payload
+            return;
+        case ADD_VALUE_TO_COUNT:
+            state.count = state.count + state.valueToAdd
+            state.valueToAdd = 0
+            return;
+        default:
+            return;
+    }
+}
 
 export default function CounterPage({ initialCount }) {
-    const { count, incrementCounter } = useCounter(initialCount);
+    const [state, dispatch] = useReducer(produce(reducer), {
+        count: initialCount,
+        valueToAdd: 0
+    })
+    function increment() {
+        dispatch({
+            type: INCREMENT_COUNT,
+            payload: 1
+        })
+    }
+    function deecrement() {
+        dispatch({
+            type: DECREMENT_COUNT,
+            payload: 1
+        })
+    }
+
+    function handleChange(evt) {
+        const value = parseInt(evt.target.value) || 0;
+        dispatch({
+            type: SET_VALUE_TO_ADD,
+            payload: value
+        })
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        dispatch({ type: ADD_VALUE_TO_COUNT })
+    }
 
     return (
-        <div>
-            <h1>Count is {count}</h1>
-            <Button
-                onClick={incrementCounter}
-            >
-                Increment
-            </Button>
-        </div>
+        <Panel className="m-3">
+            <h1 className="text-xl">Count is {state.count}</h1>
+            <div className="flex flex-row">
+                <Button onClick={increment}>Increment</Button>
+                <Button onClick={deecrement}>Decrement</Button>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot!</label>
+                <input
+                    value={state.valueToAdd || ""}
+                    onChange={handleChange}
+                    type="number"
+                    className="p-1 m-3 bg-gray-50 border border-gray-300"
+                />
+                <Button>Add it</Button>
+            </form>
+        </Panel>
     )
 }
