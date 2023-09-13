b01adea662e140ce19ff877a52f51b4b204efc3c
Rupesh Biswas <rupeshbiswas1997@gmail.com>
2023-09-13 20:35:23 +0530

    removed an webpack error handler

--- React-and-Typescript/jbook-using-vite/packages/local-client/src/components/resizable.tsx
+++ React-and-Typescript/jbook-using-vite/packages/local-client/src/components/resizable.tsx
@@ -17,26 +17,6 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const [width, setWidth] = useState(window.innerWidth * 0.75);
 
-  // Handling ResizeObserver loop error. Will handle it in a good way in future if found something
-  useEffect(() => {
-    const resizeErrorListener = (e: ErrorEvent) => {
-      //   prettier-ignore
-      if (e.message ==="ResizeObserver loop completed with undelivered notifications.") {
-              const resizeObserverErrDiv = document.getElementById("webpack-dev-server-client-overlay-div");
-              const resizeObserverErr = document.getElementById("webpack-dev-server-client-overlay");
-              if (resizeObserverErr) {
-                resizeObserverErr.setAttribute("style", "display: none");
-              }
-              if (resizeObserverErrDiv) {
-                resizeObserverErrDiv.setAttribute("style", "display: none");
-              }
-            }
-    };
-    window.addEventListener("error", resizeErrorListener);
-
-    return () => window.removeEventListener("error", resizeErrorListener);
-  }, []);
-
   useEffect(() => {
     let timer: any;
     const listener = () => {
