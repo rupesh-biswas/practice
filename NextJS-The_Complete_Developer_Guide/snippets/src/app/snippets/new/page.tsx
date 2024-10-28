c00c2618120a696fef76ff107ade396b315be866
Rupesh biswas <rupesh.biswas@soroco.com>
2024-10-28 20:02:25 +0530

    added captacha

--- NextJS-The_Complete_Developer_Guide/snippets/src/app/snippets/new/page.tsx
+++ NextJS-The_Complete_Developer_Guide/snippets/src/app/snippets/new/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import * as actions from "@/actions";
+import { useState } from "react";
 import { useFormState } from "react-dom";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function SnippetCreatePage() {
   const [formState, action] = useFormState(actions.createSnippet, {
     message: "",
   });
+  const [captchaVerified, setCaptchaVerified] = useState(false);
 
+  function handleCaptachVerification(token: string | null) {
+    setCaptchaVerified(!!token);
+  }
   return (
     <form action={action}>
       <h3 className="m-3 font-bold">Create a Snippet</h3>
@@ -39,9 +45,15 @@ export default function SnippetCreatePage() {
           </div>
         ) : null}
 
+        <ReCAPTCHA
+          sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
+          onChange={handleCaptachVerification}
+        />
+
         <button
           type="submit"
-          className="ml-12 rounded bg-blue-200 p-2 hover:bg-blue-300 active:bg-blue-400"
+          disabled={!captchaVerified}
+          className={`ml-12 rounded p-2 ${captchaVerified ? "bg-blue-200 hover:bg-blue-300 active:bg-blue-400" : "cursor-not-allowed bg-gray-200"}`}
         >
           Create
         </button>
